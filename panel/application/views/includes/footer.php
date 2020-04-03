4ee812ada4a91e9017db6c9c9ae95ce624a40384
HuseyinKadioglu <infohuseyinkadioglu@gmail.com>
2020-04-04 00:52:58 +0300

    dashboard_v için bir klasör oluşturuldu.Boş content sayfası yapıldı

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,12 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+        <div class="clearfix">
+            <ul class="footer-menu pull-right">
+                <li><a href="javascript:void(0)">Mağazalarımız</a></li>
+                <li><a href="javascript:void(0)">İletişim</a></li>
+                <li><a href="javascript:void(0)">Sosyal Medya <i class="fa fa-angle-up m-l-md"></i></a></li>
+            </ul>
+            <div class="copyright pull-left">Copyright Hüseyin Kadıoğlu <?php echo date("Y"); ?> &copy;</div>
+        </div>
+    </footer>
+</div>
